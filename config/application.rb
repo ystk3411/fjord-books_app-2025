fcd039b0aa6e4d33fe2be10d2d82066071a5b733
ystk3411 <yoshi.hama0701@gmail.com>
2026-07-11 22:09:49 +0900

    en.ymlを修正

--- config/application.rb
+++ config/application.rb
@@ -8,7 +8,7 @@ Bundler.require(*Rails.groups)
 
 module BooksApp
   class Application < Rails::Application
-    config.i18n.default_locale = :ja
+    config.i18n.default_locale = :en
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 8.0
 
