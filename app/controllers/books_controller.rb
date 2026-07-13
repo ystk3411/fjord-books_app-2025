51003b4e9f0e8c09ee89247d23f26a246e0aa23d
ystk3411 <yoshi.hama0701@gmail.com>
2026-07-13 19:33:47 +0900

    ページングを実装

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -5,7 +5,8 @@ class BooksController < ApplicationController
 
   # GET /books or /books.json
   def index
-    @books = Book.all
+    # @books = Book.all
+    @books = Book.order(created_at: :desc).page(params[:page]).per(5)
   end
 
   # GET /books/1 or /books/1.json
