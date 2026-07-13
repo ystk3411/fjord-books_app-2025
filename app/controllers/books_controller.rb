f342e0e9f949456d938028a92417617104496801
ystk3411 <yoshi.hama0701@gmail.com>
2026-07-13 23:53:11 +0900

    コメントを削除

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -5,7 +5,6 @@ class BooksController < ApplicationController
 
   # GET /books or /books.json
   def index
-    # @books = Book.all
     @books = Book.order(created_at: :desc).page(params[:page]).per(5)
   end
 
