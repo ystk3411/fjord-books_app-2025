94b4b4475cb33d946e0040e3a112babf23efceda
ystk3411 <yoshi.hama0701@gmail.com>
2026-07-16 18:33:10 +0900

    ユーザー一覧画面を実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,10 @@
 class UsersController < ApplicationController
   def index
+    @users = User.order(:id).page(params[:page])
+    p @users
   end
 
   def show
+    @user = User.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
-  get "users/index"
-  get "users/show"
   devise_for :users
   resources :books
-  
+  resources :users, only: [:index, :show]
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
