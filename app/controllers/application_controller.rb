e053bdb7b540c2a9154890c53302bfabb354f558
ystk3411 <yoshi.hama0701@gmail.com>
2026-07-16 17:46:18 +0900

    Usersコントローラを追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,5 +2,6 @@
 
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
+  before_action :authenticate_user!
   allow_browser versions: :modern
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
+  get "users/index"
+  get "users/show"
   devise_for :users
   resources :books
+  
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
