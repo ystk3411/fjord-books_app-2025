07ce02ae5210eb452d2ff9bfb1572aaebe184b6e
ystk3411 <yoshi.hama0701@gmail.com>
2026-07-16 17:15:19 +0900

    Userテーブルを追加

--- db/migrate/20260716075706_devise_create_users.rb
+++ db/migrate/20260716075706_devise_create_users.rb
@@ -6,6 +6,10 @@ class DeviseCreateUsers < ActiveRecord::Migration[8.0]
       ## Database authenticatable
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
+      t.string :post_code
+      t.string :prefecture_code
+      t.string :city
+      t.string :street
 
       ## Recoverable
       t.string   :reset_password_token
