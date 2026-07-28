75e774fec25c73ee2d3e66f79982157caa4394ce
ystk3411 <yoshi.hama0701@gmail.com>
2026-07-28 23:38:33 +0900

    ユーザー編集画面をDeviseのデフォルトのものへ変更

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,5 +3,12 @@
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
   allow_browser versions: :modern
+
+  private
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:account_update, keys: %i[post_code prefecture_code city street])
+  end
 end
