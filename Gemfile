d70c6bfc77032df29952ce8fdbce368e9a8e2231
ystk3411 <yoshi.hama0701@gmail.com>
2026-08-12 22:26:59 +0900

    rubocopを実行

--- Gemfile
+++ Gemfile
@@ -75,5 +75,5 @@ end
 gem 'carrierwave'
 gem 'kaminari'
 
-gem "devise", "~> 5.0"
+gem 'devise', '~> 5.0'
 gem 'devise-i18n'
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
+# frozen_string_literal: true
+
 class UsersController < ApplicationController
-  before_action :set_user, only: %i[show edit update ]
+  before_action :set_user, only: %i[show edit update]
 
   def index
     @users = User.order(:id).page(params[:page])
