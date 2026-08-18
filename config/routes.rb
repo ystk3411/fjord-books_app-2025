5e439b31d8fd8b8a03b821d6998f68d5e039d248
ystk3411 <yoshi.hama0701@gmail.com>
2026-08-18 22:27:39 +0900

    commentsコントローラを作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get "comments/index"
+  get "comments/show"
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
   devise_for :users
   root to: 'books#index'
