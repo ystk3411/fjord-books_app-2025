b42f60c282dc92355733b95de6aef775eafd11b5
ystk3411 <yoshi.hama0701@gmail.com>
2026-08-24 20:00:05 +0900

    指摘点を修正20260824

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,14 +16,10 @@ class UsersController < ApplicationController
   end
 
   def update
-    respond_to do |format|
-      if @user.update(user_params)
-        format.html { redirect_to @user, notice: t('controllers.common.notice_update', name: User.model_name.human) }
-        format.json { render :show, status: :ok, location: @user }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
-      end
+    if @user.update(user_params)
+      redirect_to @user, notice: t('controllers.common.notice_update', name: User.model_name.human)
+    else
+      render :edit, status: :unprocessable_entity
     end
   end
 
