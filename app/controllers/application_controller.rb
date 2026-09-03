2a953fd0f1c7f242855da6dd466c28fe5e63cfb0
ystk3411 <yoshi.hama0701@gmail.com>
2026-09-04 00:30:37 +0900

    deviseのストロングパラメータのメソッドを修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,8 +10,9 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :postal_code, :address, :self_introduction, :icon])
-    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :postal_code, :address, :self_introduction, :icon])
+    keys = %i[name postal_code address self_introduction icon]
+    devise_parameter_sanitizer.permit(:sign_up, keys:)
+    devise_parameter_sanitizer.permit(:account_update, keys:)
   end
 
   private
