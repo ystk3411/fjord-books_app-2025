199bd0e73f3b4101442da6bec74573e2d19566ff
ystk3411 <yoshi.hama0701@gmail.com>
2026-09-08 22:46:17 +0900

    言及のリレーションを設定

--- app/models/mention.rb
+++ app/models/mention.rb
@@ -1,2 +1,4 @@
 class Mention < ApplicationRecord
+  belongs_to :sender, class_name: 'Report'
+  belongs_to :receiver, class_name: 'Report'
 end
--- app/models/report.rb
+++ app/models/report.rb
@@ -3,6 +3,10 @@
 class Report < ApplicationRecord
   belongs_to :user
   has_many :comments, as: :commentable, dependent: :destroy
+  has_many :passive_mentions, class_name: 'Mention', foreign_key: 'receiver_id', dependent: :destroy
+  has_many :mentioning_reports, through: :passive_mentions, source: :sender
+  has_many :active_mentions, class_name: 'Mention', foreign_key: 'sender_id', dependent: :destroy
+  has_many :mentioned_reports, through: :active_mentions, source: :receiver
 
   validates :title, presence: true
   validates :content, presence: true
