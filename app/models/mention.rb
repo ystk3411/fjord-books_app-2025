296c01bfc7ad5acd6fe0fb1aa3838758445677d6
ystk3411 <yoshi.hama0701@gmail.com>
2026-09-14 22:56:20 +0900

    言及機能を実装

--- app/models/mention.rb
+++ app/models/mention.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class Mention < ApplicationRecord
   belongs_to :sender, class_name: 'Report'
   belongs_to :receiver, class_name: 'Report'
--- app/models/report.rb
+++ app/models/report.rb
@@ -3,9 +3,9 @@
 class Report < ApplicationRecord
   belongs_to :user
   has_many :comments, as: :commentable, dependent: :destroy
-  has_many :passive_mentions, class_name: 'Mention', foreign_key: 'receiver_id', dependent: :destroy
+  has_many :passive_mentions, class_name: 'Mention', foreign_key: 'receiver_id', dependent: :destroy, inverse_of: :receiver
   has_many :mentioning_reports, through: :passive_mentions, source: :sender
-  has_many :active_mentions, class_name: 'Mention', foreign_key: 'sender_id', dependent: :destroy
+  has_many :active_mentions, class_name: 'Mention', foreign_key: 'sender_id', dependent: :destroy, inverse_of: :sender
   has_many :mentioned_reports, through: :active_mentions, source: :receiver
 
   validates :title, presence: true
